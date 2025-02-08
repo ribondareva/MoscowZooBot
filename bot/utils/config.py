bd5a3e4571534801269a53f71c8fc523ec1e1092
ribondareva <msbondareva01@gmail.com>
2025-02-08 18:20:30 +0300

    Added config file

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -1,11 +1,17 @@
 # Загрузка конфигурации
-# from pydantic_settings import BaseSettings, SettingsConfigDict
-# from pydantic import SecretStr
-#
-#
-# class Settings(BaseSettings):
-#     bot_token = SecretStr
-#     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-#
-#
-# config = Settings()
+from pydantic_settings import BaseSettings  # type: ignore
+
+
+class Settings(BaseSettings):
+    bot_token: str
+    database_url: str
+    api_url: str
+    debug: bool = False  # Значение по умолчанию, если переменной нет в .env
+
+
+class Config:
+    env_file = ".env"
+    env_file_encoding = "utf-8"
+
+
+config = Settings()
